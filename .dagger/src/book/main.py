ca884a82b3eb418385a5cfdff2a5c7b1e7999d5d
Jimmy-Tempest <smartwattana@gmail.com>
2025-10-17 15:34:20 +0700

    ci: dagger functions

--- .dagger/src/book/main.py
+++ .dagger/src/book/main.py
@@ -13,10 +13,42 @@ class Book:
             .with_exec(["sh", "-c", "apt-get update && apt-get install -y libpq-dev"])
             .with_directory("/app", source)
             .with_workdir("/app")
+            .with_mounted_cache("/root/.cache/pip", dag.cache_volume("python-pip"))
             .with_exec(["pip", "install", "-r", "requirements.txt"])
             .with_exposed_port(8000)
             .with_entrypoint(["fastapi", "run", "main.py", "--port", "8000"])
         )
+
+    @function
+    async def test(self, source: dagger.Directory) -> str:
+        """Test App"""
+        postgres = (
+            dag.container()
+            .from_("postgres:15-alpine")
+            .with_env_variable("POSTGRES_USER", "app_user")
+            .with_env_variable("POSTGRES_PASSWORD", "secret")
+            .with_file(
+                "/docker-entrypoint-initdb.d/init-dbs.sh", source.file("./init-dbs.sh")
+            )
+            .with_exposed_port(5432)
+            .as_service(args=[], use_entrypoint=True)
+        )
+        return (
+            await self.env(source)
+            .with_service_binding("db", postgres)
+            .with_env_variable("DATABASE_URL", "postgresql://app_user:secret@db/app_db")
+            .with_env_variable(
+                "TEST_DATABASE_URL", "postgresql://app_user:secret@db/app_db_test"
+            )
+            .with_exec(["pytest"])
+            .stdout()
+        )
+
+    @function
+    async def publish(self, source: dagger.Directory) -> str:
+        await self.test(source)
+
+        return await self.env(source).publish(f"ttl.sh/my-fastapi-app-smartwatt")
 
     @function
     def container_echo(self, string_arg: str) -> dagger.Container:
